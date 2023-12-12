3b4436c02185eb26f4c9f2c80d68406d5fe657f8
Emerson Fonseca <idd_emefonseca@uolinc.com>
2023-12-12 17:55:18 -0300

    Initial commit

---
+++ src/main/java/com/ambev/projetopratico5/ProjetoPratico5Application.java
@@ -0,0 +1,13 @@
+package com.ambev.projetopratico5;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoPratico5Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoPratico5Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/ambev/projetopratico5/ProjetoPratico5ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ambev.projetopratico5;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoPratico5ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
